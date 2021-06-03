95a258b8d82ce1a2d793bcb61cf9e260197bede7
Александр Никитин <walter@MacBook-Air-Aleksandr.local>
2021-06-04 00:59:21 +0600

    Выполнил обязательно домашнее задание №6

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
-console.log('test');
+let num = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+let hiddenNum = num (1, 100);
+console.log(hiddenNum);
+let userNum;
+
+let isNumber = function (n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+let userQuestion = function  (question) {
+  userNum = prompt(question);
+  if ( userNum === null ) {
+    alert('Игра окончена')
+  } else {
+    userNum = +userNum
+    if (isNumber(userNum)) {
+      if ( hiddenNum < userNum ) {
+        userQuestion('Заданное число меньше');
+      } else if ( hiddenNum > userNum) {
+        userQuestion('Заданное число больше');
+      } else {
+        alert('Поздравляю, Вы угадали!!!')
+      }
+    } else {
+      userQuestion('Введи число!')
+    }
+  }
+}
+
+userQuestion('Загадывание случайного числа от 1 до 100');
