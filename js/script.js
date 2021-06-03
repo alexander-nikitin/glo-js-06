efe726ef54bec547e22bb9cef0e9b244ae60c142
Александр Никитин <walter@MacBook-Air-Aleksandr.local>
2021-06-04 01:23:29 +0600

    Сделал усложненное домашнее задание для урока №6

--- js/script.js
+++ js/script.js
@@ -5,31 +5,54 @@ let num = function (min, max) {
 };
 
 let hiddenNum = num (1, 100);
-console.log(hiddenNum);
+console.log('Загаданное число: ' + hiddenNum);
 let userNum;
 
-let isNumber = function (n) {
+let isNumber = function (n, iter) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
-let userQuestion = function  (question) {
+let userQuestion = function  (question, iter) {
   userNum = prompt(question);
+  iteration = iter;
+  console.log(iteration);
+
   if ( userNum === null ) {
-    alert('Игра окончена')
+    alert('Игра окончена');
   } else {
-    userNum = +userNum
-    if (isNumber(userNum)) {
-      if ( hiddenNum < userNum ) {
-        userQuestion('Заданное число меньше');
-      } else if ( hiddenNum > userNum) {
-        userQuestion('Заданное число больше');
+
+    if (iteration == 0) {
+      let again = confirm('Попытки закончились, хотите сыграть еще?');
+      if ( again ) {
+        hiddenNum = num (1, 100);
+        console.log('Загаданное число: ' + hiddenNum);
+        userQuestion('Загадывание случайного числа от 1 до 100', 9);
       } else {
-        alert('Поздравляю, Вы угадали!!!')
+        alert('Игра окончена');
       }
     } else {
-      userQuestion('Введи число!')
+      userNum = +userNum
+      if (isNumber(userNum)) {
+        if ( hiddenNum < userNum ) {
+          userQuestion('Заданное число меньше, осталось попыток: ' + iteration, iteration - 1);
+        } else if ( hiddenNum > userNum) {
+          userQuestion('Заданное число больше, осталось попыток: ' + iteration, iteration - 1);
+        } else {
+          win = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+          if ( win ) {
+            hiddenNum = num (1, 100);
+            console.log('Загаданное число: ' + hiddenNum);
+            userQuestion('Загадывание случайного числа от 1 до 100', 9);
+          } else {
+            alert('Игра окончена');
+          }
+        }
+      } else {
+        userQuestion('Введи число! Осталось попыток: ' + iteration, iteration - 1)
+      }
     }
+
   }
 }
 
-userQuestion('Загадывание случайного числа от 1 до 100');
+userQuestion('Загадывание случайного числа от 1 до 100', 9);
